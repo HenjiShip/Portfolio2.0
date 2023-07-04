55657d34d8e7b9856618d59a43ba7d24d36c03df
unknown <lihenry.cs@gmail.com>
2023-07-03 20:55:56 -0400

    added projects section layount

--- pages/Portfolio.jsx
+++ pages/Portfolio.jsx
@@ -1,11 +1,64 @@
 import React from "react";
+import Projects from "../components/Projects";
 import { useStateContext } from "../context/StateContext";
+import drizco from "../assets/drizco.png";
+import drizcoGIF from "../assets/drizco.gif";
 
 const Portfolio = () => {
-  const {project} = useStateContext()
+  const { projectScroll } = useStateContext();
+  const projects = [
+    {
+      name: "Drizco",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard",
+    },
+    {
+      name: "Stickstock",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard too",
+    },
+    {
+      name: "Chateth",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard also",
+    },
+    {
+      name: "Gluetube",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard also",
+    },
+    {
+      name: "Portfolio v1.0",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard also",
+    },
+    {
+      name: "Movie Search App",
+      coverImage: drizco,
+      coverGIF: drizcoGIF,
+      images: ["image1", "image2", "image3"],
+      description: "I hate mustard also",
+    },
+  ];
   return (
     <div className="portfolio">
-      <div ref={project} className="container">Portfolio</div>
+      <div ref={projectScroll} className="container">
+        <div className="grid-container">
+          {projects.map((proj) => {
+            return <Projects proj={proj} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
